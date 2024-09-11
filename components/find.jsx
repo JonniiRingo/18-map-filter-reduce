d0601332100ef222a08ffd4c9a47c1481f2c36cb
Jonnii Ringo <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-09-11 15:42:29 -0700

    find function complete

--- components/find.jsx
+++ components/find.jsx
@@ -1,9 +1,13 @@
 import React from "react"; 
+import emojipedia from "../emojipedia";
 
 
 
 function findEmoji(){
-    return 
+    return emojipedia.find((element) => element > 3); 
 }
 
 export default findEmoji; 
+
+
+
